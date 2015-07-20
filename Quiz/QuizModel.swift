c053d1c3d3d798c7f0ef5089e346fd9ecf1e7d9d
Toph <toph.matta@gmail.com>
2015-07-20 14:04:48 -0500

    Parsing JSON file pt. 1

--- Quiz/QuizModel.swift
+++ Quiz/QuizModel.swift
@@ -12,17 +12,41 @@ class QuizModel: NSObject {
     
     func getQuestions() -> [Question] {
         
+        // Array of question objects
         var questions:[Question] = [Question]()
         
-        // to do: Get JSON file
+        // Get JSON array of dictionaries
         
         
-        // to do: Parse JSON file
+        // Loop through each dictionary and assign values to our question objects
         
         
         // Return list of question objects
-        
         return questions
     }
    
+    func getLocaLJsonFile() -> [NSDictionary] {
+        
+        // Get and NSURL object pointing to the json file in our app bundle
+        let appBundlePath:String? = NSBundle.mainBundle().pathForResource("QuestionData", ofType: "json")
+        
+        
+        // Use optional binding to check if path exists
+        if let actualBundlePath = appBundlePath {
+            
+            // Bundle path exists
+            let urlPath:NSURL? = NSURL(fileURLWithPath: actualBundlePath)
+            
+            if let actualURLPath = urlPath {
+                
+                //
+                
+            }
+            
+        }
+        else {
+            // Path to json file in the app bundle doesn't exist
+        }
+    }
+    
 }
