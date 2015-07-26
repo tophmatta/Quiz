f4b8ba75febf17ed4eedfd9af5f65c64da3f9387
Toph <toph.matta@gmail.com>
2015-07-26 16:50:49 -0500

    Setup Feedback screen overlay when question answered to display correct or incorrect

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -20,10 +20,22 @@ class ViewController: UIViewController {
     
     var currentQuestion:Question?
     var answerButtonArray:[AnswerButtonView] = [AnswerButtonView]()
+    
+    // Result view IBOutlet properties
+    @IBOutlet weak var resultTitleLabel: UILabel!
+    @IBOutlet weak var feedbackLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var resultView: UIView!
+    @IBOutlet weak var dimView: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Hide the dim and result views
+        self.dimView.alpha = 0
+        self.resultView.alpha = 0
         
         // Get the questions from the quiz model
         self.questions = self.model.getQuestions()
@@ -68,9 +80,13 @@ class ViewController: UIViewController {
             // Place it into the content view
             self.scrollViewContentView.addSubview(answer)
             
+            // Add tap gesture recognizer
+            let tapGesture:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action:Selector("answerTapped:"))
+                answer.addGestureRecognizer(tapGesture)
             
             // Add constraints depending on what number button it is
             let heightConstraint:NSLayoutConstraint = NSLayoutConstraint(item: answer, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 100)
+            answer.addConstraint(heightConstraint)
             
             let leftMarginConstraint:NSLayoutConstraint = NSLayoutConstraint(item: answer, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: self.scrollViewContentView, attribute: NSLayoutAttribute.Left, multiplier: 1, constant: 0)
             
@@ -89,11 +105,48 @@ class ViewController: UIViewController {
         }
         
         // Adjust the height of the content view so it can scroll if need be
-        let contentViewHeight:NSLayoutConstraint = NSLayoutConstraint(item: self.scrollViewContentView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: CGFloat(self.currentQuestion!.answers.count), constant: 101)
+        let contentViewHeight:NSLayoutConstraint = NSLayoutConstraint(item: self.scrollViewContentView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: self.answerButtonArray[0], attribute: NSLayoutAttribute.Height, multiplier: CGFloat(self.answerButtonArray.count - 1), constant: 101)
         
         // Add constraint to the content view
-        self.scrollViewContentView.addConstraint(contentViewHeight )
+        self.scrollViewContentView.addConstraint(contentViewHeight)
         
+    }
+    
+    func answerTapped(gesture:UITapGestureRecognizer) {
+        
+        // Get access to the answer button that was tapped
+        let answerButtonThatWasTapped:AnswerButtonView? = gesture.view as! AnswerButtonView?
+        
+        if let actualButton = answerButtonThatWasTapped {
+            
+            // We got the button, now find out what index it was
+            let answerTappedIndex:Int? = find(self.answerButtonArray, actualButton)
+            
+            if let foundAnswerIndex = answerTappedIndex {
+                
+                // Compare the answer index that was tapped vs the correct index from question
+                if (foundAnswerIndex == self.currentQuestion!.correctAnswerIndex) {
+                    
+                    // User got it correct
+                    self.resultTitleLabel.text = "Correct"
+                    
+                }
+                else {
+                    
+                    // User got it wrong
+                    self.resultTitleLabel.text = "Incorrect"
+                    
+                }
+                
+                // Set the feedback text
+                self.feedbackLabel.text = self.currentQuestion!.feedback
+                
+                // Display the dimView and the resultView
+                self.dimView.alpha = 1
+                self.resultView.alpha = 1
+                
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
